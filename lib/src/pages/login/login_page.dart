8f55374a8f626407ebd57fc42c2c1a9e750ac156
Tanuphong Phoemtaweesantisuh <tanuphong.p@hotmail.com>
2021-03-23 09:40:11 +0700

    prepare login page

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -20,16 +20,77 @@ class LoginPage extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text('Header'),
-              Text('login'),
-              Text('login button'),
-              Text('forgot password'),
-              Text('sso'),
-            ],
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 48,),
+                Image.asset(
+                  'assets/images/cdgs-logo.png',
+                  width: 300,
+                ),
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Card(
+                      margin: EdgeInsets.only(
+                        bottom: 24,
+                        left: 24,
+                        right: 24,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 22, top: 22, right: 22, bottom: 62),
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: 'example@gmail.com',
+                                labelText: 'username',
+                                icon: Icon(Icons.person),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                            Divider(
+                              height: 28,
+                              indent: 22,
+                              endIndent: 22,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                labelText: 'password',
+                                icon: Icon(Icons.lock),
+                                border: InputBorder.none,
+                              ),
+                              obscureText: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 280,
+                      height: 52,
+                      child: ElevatedButton(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: () {
+                          print('login click!!!!');
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                Text('login button'),
+                Text('forgot password'),
+                Text('sso'),
+              ],
+            ),
           ),
         ],
       ),
