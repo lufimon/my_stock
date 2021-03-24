860fff66560e28e58b5f3a16874fefd3e183e0dd
Tanuphong Phoemtaweesantisuh <tanuphong.p@hotmail.com>
2021-03-24 08:59:03 +0700

    grid view

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_stock/src/configs/app_route.dart';
 import 'package:my_stock/src/pages/login/background_theme.dart';
 import 'package:my_stock/src/view_models/menu_viewmodel.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,22 +12,17 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'tanuphong:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2
         ),
+        itemBuilder: (context, index) => ShopListItem(200),
+        itemCount: 100,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          //todo
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
@@ -93,6 +76,119 @@ class CommonDrawer extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ShopListItem extends StatelessWidget {
+
+  final Function press;
+  final double maxHeight;
+
+  const ShopListItem(this.maxHeight, {Key key, this.press})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            _buildImage(),
+            Expanded(
+              child: _buildInfo(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding _buildInfo() => Padding(
+    padding: EdgeInsets.all(6),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'ชื่อสินค้า',
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              '\$ ราคาสินค้า',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'จำนวน',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrangeAccent,
+              ),
+            )
+          ],
+        ),
+      ],
+    ),
+  );
+
+  Stack _buildImage() {
+    final height = maxHeight - 75;
+    final productImage = 'https://shortrecap.co/wp-content/uploads/2020/05/Catcover_web.jpg';
+    return Stack(
+      children: [
+        productImage != null && productImage.isNotEmpty
+            ? Image.network(
+          productImage,
+          height: height,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        )
+            : Image.asset(
+          'xxx',
+          height: height,
+          width: double.infinity,
+        ),
+        1 > 0
+            ? SizedBox()
+            : Positioned(
+          top: 1,
+          right: 1,
+          child: Card(
+            color: Colors.white70,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+                vertical: 4,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.box,
+                    size: 15.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    'out of stock',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
