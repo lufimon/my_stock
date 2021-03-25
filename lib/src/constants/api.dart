91f2d06bf536c0ba4fa920d9b28b8fd728fc080e
Tanuphong Phoemtaweesantisuh <tanuphong.p@hotmail.com>
2021-03-25 09:49:19 +0700

    open camera

--- lib/src/constants/api.dart
+++ lib/src/constants/api.dart
@@ -1,5 +1,5 @@
 class API {
-  static const String BASE_URL = 'https://d92e1eb74350.jp.ngrok.io';
+  static const String BASE_URL = 'https://e6b29e8e7f3c.jp.ngrok.io';
   static const String IMAGE_URL = '$BASE_URL/images';
   static const String PRODUCT = '/products';
 }
