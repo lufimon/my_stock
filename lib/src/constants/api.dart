0b077a7c24496a63a2cde139117c204e7b816fc9
Tanuphong Phoemtaweesantisuh <tanuphong.p@hotmail.com>
2021-03-25 15:18:46 +0700

    edit product with image

--- lib/src/constants/api.dart
+++ lib/src/constants/api.dart
@@ -1,5 +1,5 @@
 class API {
-  static const String BASE_URL = 'https://c5aa5e362aa6.jp.ngrok.io';
+  static const String BASE_URL = 'https://6051598f4143.jp.ngrok.io';
   static const String IMAGE_URL = '$BASE_URL/images';
   static const String PRODUCT = '/products';
 }
--- lib/src/pages/management/management_page.dart
+++ lib/src/pages/management/management_page.dart
@@ -4,6 +4,7 @@ import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:my_stock/src/constants/api.dart';
 import 'package:my_stock/src/models/product_response.dart';
 import 'package:my_stock/src/services/network.dart';
 
@@ -25,6 +26,10 @@ class _ManagementPageState extends State<ManagementPage> {
     _editMode = false;
     _product = ProductResponse();
     super.initState();
+  }
+
+  callback(File image){
+    _image = image;
   }
 
   @override
@@ -59,6 +64,7 @@ class _ManagementPageState extends State<ManagementPage> {
                     )
                   ],
                 ),
+                ProductImage(callback, image: _product.image)
               ],
             ),
           ),
@@ -117,7 +123,7 @@ class _ManagementPageState extends State<ManagementPage> {
               FocusScope.of(context).requestFocus(FocusNode());
               if (_editMode) {
                 try {
-                  final message = await NetworkService().editProduct(null, _product);
+                  final message = await NetworkService().editProduct(_image, _product);
                   Navigator.pop(context);
                   showAlertBar(
                     message,
@@ -132,7 +138,7 @@ class _ManagementPageState extends State<ManagementPage> {
               } else {
                 try {
                   final message =
-                      await NetworkService().addProduct(null, _product);
+                      await NetworkService().addProduct(_image, _product);
                   Navigator.pop(context);
                   showAlertBar(
                     message,
@@ -174,4 +180,154 @@ class _ManagementPageState extends State<ManagementPage> {
       flushbarStyle: FlushbarStyle.GROUNDED,
     )..show(context);
   }
+}
+
+
+class ProductImage extends StatefulWidget {
+  final Function callBack;
+  final String image;
+
+  const ProductImage(this.callBack, {Key key, @required this.image})
+      : super(key: key);
+
+  @override
+  _ProductImageState createState() => _ProductImageState();
+}
+
+class _ProductImageState extends State<ProductImage> {
+  File _imageFile;
+  String _image;
+  final _picker = ImagePicker();
+
+  @override
+  void initState() {
+    _image = widget.image;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _imageFile?.delete();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildPickerImage(),
+          _buildPreviewImage(),
+        ],
+      ),
+    );
+  }
+
+  dynamic _buildPreviewImage() {
+    if ((_image == null || _image.isEmpty) && _imageFile == null) {
+      return SizedBox();
+    }
+
+    final container = (Widget child) => Container(
+      color: Colors.grey[100],
+      margin: EdgeInsets.only(top: 4),
+      alignment: Alignment.center,
+      height: 350,
+      child: child,
+    );
+
+    return _image != null
+        ? container(Image.network('${API.IMAGE_URL}/$_image'))
+        : Stack(
+      children: [
+        container(Image.file(_imageFile)),
+        _buildDeleteImageButton(),
+      ],
+    );
+  }
+
+  OutlinedButton _buildPickerImage() => OutlinedButton.icon(
+    icon: FaIcon(FontAwesomeIcons.image),
+    label: Text('image'),
+    onPressed: () {
+      _modalPickerImage();
+    },
+  );
+
+  void _modalPickerImage() {
+    final buildListTile =
+        (IconData icon, String title, ImageSource source) => ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context).pop();
+        _pickImage(source);
+      },
+    );
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              buildListTile(
+                Icons.photo_camera,
+                "Take a picture from camera",
+                ImageSource.camera,
+              ),
+              buildListTile(
+                Icons.photo_library,
+                "Choose from photo library",
+                ImageSource.gallery,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _pickImage(ImageSource source) {
+    _picker
+        .getImage(
+      source: source,
+      imageQuality: 70,
+      maxHeight: 500,
+      maxWidth: 500,
+    )
+        .then((file) {
+      if (file != null) {
+        setState(() {
+          _imageFile = File(file.path);
+          _image = null;
+          widget.callBack(_imageFile);
+        });
+      }
+    }).catchError((error) {
+      //todo
+    });
+  }
+
+
+  Positioned _buildDeleteImageButton() => Positioned(
+    right: 0,
+    child: IconButton(
+      onPressed: () {
+        setState(() {
+          _imageFile = null;
+          widget.callBack(null);
+        });
+      },
+      icon: Icon(
+        Icons.clear,
+        color: Colors.black54,
+      ),
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+    ),
+  );
 }
