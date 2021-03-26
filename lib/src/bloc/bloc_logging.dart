838666538c5973d33a3cf07c167d52d47fc52e0d
Tanuphong Phoemtaweesantisuh <tanuphong.p@hotmail.com>
2021-03-26 15:30:08 +0700

    add icon

---
+++ lib/src/bloc/bloc_logging.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BlocLogging extends BlocObserver {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+}
