b6c97be119872b2c85249014ccbf713a526fea6a
Tanuphong Phoemtaweesantisuh <tanuphong.p@hotmail.com>
2021-03-26 09:57:38 +0700

    redesign

--- lib/src/pages/map/googlemap_page.dart
+++ lib/src/pages/map/googlemap_page.dart
@@ -33,25 +33,36 @@ class GoogleMapPageState extends State<GoogleMapPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text('Map'),
+        title: Text('Google Map'),
       ),
-      body: GoogleMap(
-        markers: _marker,
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      body: Stack(
+        children: [
+          GoogleMap(
+            markers: _marker,
+            mapType: MapType.hybrid,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+          Positioned(
+            left: 8,
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(8),
+                primary: Colors.black,
+              ),
+              onPressed: _pinMarker,
+              label: Text('Pin Biker'),
+              icon: Icon(Icons.pin_drop),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _pinMarker() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_codeMobile));
     dummyData.forEach((element) {
